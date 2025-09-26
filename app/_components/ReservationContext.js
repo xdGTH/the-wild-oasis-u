33da809303ba781e5dc0cdf95bc1ea60a10ea5fb
xdGTH <gotohell.heaven45@gmail.com>
2025-09-26 20:20:57 +0545

    Fixed context value

--- app/_components/ReservationContext.js
+++ app/_components/ReservationContext.js
@@ -10,7 +10,7 @@ function ReservationProvider({ children }) {
   const [range, setRange] = useState(initialState);
   const resetRange = () => setRange(initialState);
   return (
-    <ReservationContext.Provider value={(range, setRange, resetRange)}>
+    <ReservationContext.Provider value={{ range, setRange, resetRange }}>
       {children}
     </ReservationContext.Provider>
   );
