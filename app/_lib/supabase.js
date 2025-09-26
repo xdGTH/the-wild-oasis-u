b15b79bf3fdfdf15ea0e704d1c975045baf15c73
xdGTH <gotohell.heaven45@gmail.com>
2025-09-26 18:27:43 +0545

    Issues

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SPUABASE_KEY
+  process.env.SUPABASE_KEY
 );
 
 export default supabase;
