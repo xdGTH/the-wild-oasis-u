a1d2a6943227b0c64352eb33228d6927314cd5bc
xdGTH <gotohell.heaven45@gmail.com>
2025-09-26 20:09:43 +0545

    Import fix

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -2,9 +2,9 @@
 
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
-import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
 import { redirect } from "next/dist/server/api-utils";
+import supabase from "./supabase";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
