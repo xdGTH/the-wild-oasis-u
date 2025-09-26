92bcc09eb3f73b4f854df5511ccc6c4bc8273121
xdGTH <gotohell.heaven45@gmail.com>
2025-09-26 18:16:10 +0545

    Fixed some imports

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@
 //   return NextResponse.redirect(new URL("/about", request.url));
 // }
 
-import { auth } from "@app/_lib/auth";
+import { auth } from "@/app/_lib/auth";
 
 export const middleware = auth;
 
